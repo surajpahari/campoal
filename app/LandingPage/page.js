ebf19eb40f29caf72e171660aa4b041e992c3566
surajpahari <surajpahari152@gmail.com>
2024-06-12 12:35:09 +0545

    intro section

---
+++ app/LandingPage/page.js
@@ -0,0 +1,12 @@
+const { default: IntroSection } = require("./components/IntroSection");
+
+const LandingPage = () => {
+  return (
+    <>
+      <div>
+        <IntroSection />
+      </div>
+    </>
+  );
+}
+export default LandingPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'dark-blue': '#2B2B39',
+        'orange-ash': '#F5EFE0'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,4 +19,22 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+///** @type {import('tailwindcss').Config} */
+//module.exports = {
+//  content: [
+//    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//  ],
+//  theme: {
+//    extend: {
+//      backgroundImage: {
+//        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//        "gradient-conic":
+//          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//      },
+//    },
+//  },
+//  plugins: [],
+//};
